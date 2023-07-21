611e872e59ad7d18e39cbe196bc22f47623765c2
Jose Daniel Hernandez Osorio <josedaniel.hernandez.osorio@bbva.com>
2023-07-20 19:32:02 -0600

    add docker environment work and integrated con backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 
 import './App.css';
 
+const client = axios.create({
+    baseURL: 'http://127.0.0.1:1234'
+});
+
 function App() {
+
+    useEffect(() => {
+        client.get('/imatpro/api/v1.0.0/mathematical/state/')
+            .then(res => {
+                console.log(res)
+            })
+    }, []);
+
   return (
       <div>
         <Header />
